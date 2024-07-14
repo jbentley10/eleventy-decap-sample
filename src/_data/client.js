e76d74b5b67e7ef064c427188bb18e6208f1ce92
John Bentley <jwbentle@gmail.com>
2024-07-14 16:35:20 -0700

    Update client info

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Awesome Landscaping",
+    "email": "info@awesomelandscaping.com",
+    "phoneForTel": "760-404-6942",
+    "phoneFormatted": "(760) 404-6942",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "123 Fake Street",
+        "lineTwo": "",
+        "city": "Fakesville",
+        "state": "CA",
+        "zip": "92234",
         "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
     },
     "socials": {
@@ -16,5 +16,5 @@ module.exports = {
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.awesomelandscaping.com"
 };
